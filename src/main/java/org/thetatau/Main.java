29eb9ab0361a3c6eec168eb9dda74724f7806384
Grant Sweeney <grantsweeney02@gmail.com>
2024-04-05 00:19:37 -0400

    added more checks for running certain parts of algo

--- src/main/java/org/thetatau/Main.java
+++ src/main/java/org/thetatau/Main.java
@@ -16,7 +16,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        JSONObject jsonData = JsonUtil.readJsonFromFile("src/main/resources/tester.json");
+        JSONObject jsonData = JsonUtil.readJsonFromFile("src/main/resources/tester_complex_unequal.json");
 
         Map<String, Person> bigs = PersonUtil.createPeopleFromJson(jsonData.getJSONObject("bigs"));
         Map<String, Person> littles = PersonUtil.createPeopleFromJson(jsonData.getJSONObject("littles"));
@@ -30,7 +30,7 @@ public class Main {
         }
 
         MatchingAlgorithm.runMyAlgorithm(bigs, littles);
-        System.out.println("\nAfter little's' Preferences");
+        System.out.println("\nAfter littles' Preferences");
         for (String s: bigs.keySet()) {
             System.out.println(bigs.get(s));
         }
@@ -39,11 +39,24 @@ public class Main {
         }
         HashMap<String, Person> unMatchedBigs = PersonUtil.findUnMatchedPeople(bigs);
         HashMap<String, Person> unMatchedLittles = PersonUtil.findUnMatchedPeople(littles);
-        MatchingAlgorithm.runMyAlgorithm(unMatchedLittles, unMatchedBigs);
+        if(!unMatchedBigs.isEmpty() || !unMatchedLittles.isEmpty()) {
+            MatchingAlgorithm.runMyAlgorithm(unMatchedLittles, unMatchedBigs);
+            System.out.println("\nAfter bigs' Preferences");
+            for (String s: bigs.keySet()) {
+                System.out.println(bigs.get(s));
+            }
+            for (String s: littles.keySet()) {
+                System.out.println(littles.get(s));
+            }
+        }
 
         unMatchedBigs = PersonUtil.findUnMatchedPeople(bigs);
         unMatchedLittles = PersonUtil.findUnMatchedPeople(littles);
-        MatchingAlgorithm.runRandomAlgorithm(unMatchedBigs, unMatchedLittles);
+
+        if(!unMatchedBigs.isEmpty() || !unMatchedLittles.isEmpty()) {
+            MatchingAlgorithm.runRandomAlgorithm(unMatchedBigs, unMatchedLittles);
+        }
+
 
         System.out.println("\nFinal Matching");
         for (String s: bigs.keySet()) {
